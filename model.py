14a6113ef536f068fea02de5e500de17002b3b18
Tiger Wang <tigerwang@outlook.com>
2017-11-26 17:24:53 -0500

    First drive

--- model.py
+++ model.py
@@ -15,24 +15,24 @@ IMAGES = []
 MEASUREMENTS = []
 
 for data_path in DATA_PATH_LIST:
-    with open(data_path + '\\driving_log.csv') as csvfile:
+    csv_path = data_path + '\\driving_log.csv'
+    with open(csv_path) as csvfile:
+        print('Reading image file listed in \'' +
+              csv_path + '\' and the measurements', end='')
+
         reader = csv.reader(csvfile)
         for line in reader:
             # image
             source_path = line[0]
             filename = source_path.split('\\')[-1]
             current_path = data_path + '\\IMG\\' + filename
-            print('Reading image file \'' + current_path + '\'')
             image = cv2.imread(current_path)
             assert image.shape == (160, 320, 3)
             IMAGES.append(image)
-
             # measurement
             measurement = float(line[3])
             MEASUREMENTS.append(measurement)
-
-plt.imshow(cv2.cvtColor(IMAGES[0], cv2.COLOR_BGR2RGB))
-plt.show()
+            print('.', end='')
 
 X_TRAIN = np.array(IMAGES)
 Y_TRAIN = np.array(MEASUREMENTS)
@@ -43,4 +43,7 @@ MODEL.add(Dense(1))
 
 MODEL.compile(loss='mse', optimizer='adam')
 
-MODEL.fit(X_TRAIN, Y_TRAIN, validation_split=0.2)
+MODEL.fit(X_TRAIN, Y_TRAIN, validation_split=0.2,
+          shuffle=True, nb_epoch=30, verbose=1)
+
+MODEL.save('model.h5')
